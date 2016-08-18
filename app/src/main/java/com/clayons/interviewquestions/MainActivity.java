c58d19b6c5d23c6364390ab73d6caefe7b5cbca2
Joshua Williams <joshua@max2.com>
2016-08-18 10:37:02 -0400

    Initial commit

--- app/src/main/java/com/clayons/interviewquestions/MainActivity.java
+++ app/src/main/java/com/clayons/interviewquestions/MainActivity.java
@@ -1,8 +1,13 @@
 package com.clayons.interviewquestions;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.clayons.interviewquestions.Adapters.PersonsAdapter;
 import com.clayons.interviewquestions.Model.Person;
 
 import java.util.ArrayList;
@@ -36,19 +41,72 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PERSON_DETAIL_RESULT_CODE = 100;
+
+    private List<Person> persons;
+    private PersonsAdapter personsAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        initPerson();
+
+        setUpToolbar();
+        persons = initPerson();
+        setUpRecyclerView(persons);
     }
 
-    public void initPerson() {
-        List<Person> persons = new ArrayList<>(5);
-        persons.add(new Person("John", "Doe", 20, "111-222-3333", "http://i58.tinypic.com/2z6fa6t.jpg", false));
-        persons.add(new Person("Jane", "Kish", 30, "111-222-3334", "http://i58.tinypic.com/2z6fdsl.jpg", false));
-        persons.add(new Person("Sam", "Jackson", 24, "111-222-3335", "http://i60.tinypic.com/2z6fdbr.jpg", false));
-        persons.add(new Person("Pete", "Dorey", 15, "111-222-3336", "http://i57.tinypic.com/2z6fb0p.jpg", false));
-        persons.add(new Person("George", "Mime", 5, "111-222-3337", "http://i59.tinypic.com/2z6fakl.jpg", false));
+    @SuppressWarnings("ConstantConditions")
+    private void setUpToolbar() {
+        getSupportActionBar().setTitle(R.string.persons_label);
+    }
+
+    private void setUpRecyclerView(List<Person> persons) {
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.messages_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(getAdapter(persons));
+    }
+
+    private RecyclerView.Adapter getAdapter(List<Person> persons) {
+        personsAdapter = new PersonsAdapter(persons);
+        personsAdapter.setOnPersonClickListener(goToDetailActivity());
+        return personsAdapter;
+    }
+
+    private PersonsAdapter.PersonClickListener goToDetailActivity() {
+        return new PersonsAdapter.PersonClickListener() {
+            @Override
+            public void onPersonClickListener(Person person, int position) {
+                final Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra(DetailActivity.PERSON, person);
+                intent.putExtra(DetailActivity.PERSON_POSITION, position);
+                startActivityForResult(intent, PERSON_DETAIL_RESULT_CODE);
+            }
+        };
+    }
+
+    public List<Person> initPerson() {
+        final List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Shubhanshu", "Yadav", 5, "111-222-3337", "http://www.max2.com/img/SHUBHANSHU.png", false));
+        persons.add(new Person("Atesh", "Yurdakul", 5, "111-222-3337", "http://www.max2.com/img/ATESH.png", false));
+        persons.add(new Person("Daniel", "Yurdakul", 5, "111-222-3337", "http://www.max2.com/img/DANIEL.png", false));
+        persons.add(new Person("Pranav", "Bhalla", 5, "111-222-3337", "http://www.max2.com/img/PRANAV.png", false));
+        persons.add(new Person("Rohan", "Nagrani", 20, "111-222-3333", "http://www.max2.com/img/ROHAN.png", false));
+        persons.add(new Person("Michael", "Salmasi", 30, "111-222-3334", "http://www.max2.com/img/MICHAEL.png", false));
+        persons.add(new Person("Josh", "Williams", 24, "111-222-3335", "http://www.max2.com/img/josh.png", false));
+        persons.add(new Person("Jing", "Guo", 15, "111-222-3336", "http://www.max2.com/img/jing.png", false));
+        persons.add(new Person("Zhenyu", "Wen", 5, "111-222-3337", "http://www.max2.com/img/zhenyu.png", false));
+        return persons;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK && requestCode == PERSON_DETAIL_RESULT_CODE) {
+            final Person person = data.getParcelableExtra(DetailActivity.PERSON);
+            final int position = data.getIntExtra(DetailActivity.PERSON_POSITION, -1);
+            persons.set(position, person);
+            personsAdapter.notifyItemChanged(position);
+        }
     }
 }
